b14952e26451f0f77939b5334fbc6e2d1db37e09
Ryan Wilson <42699070+rywils@users.noreply.github.com>
2025-09-29 05:40:56 -0500

    Testing Purposes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,20 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  
+  site: "https://ryanwilson.io",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
+    
   }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
